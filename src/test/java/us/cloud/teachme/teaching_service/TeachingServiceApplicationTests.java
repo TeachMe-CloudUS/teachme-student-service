dded8e41a590a489f5be1eb2a2fff56c9bc4faaa
Rami Nakkar <rami.nakkar00@gmail.com>
2024-11-12 14:35:01 +0100

    initialize project

---
+++ src/test/java/us/cloud/teachme/teaching_service/TeachingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package us.cloud.teachme.teaching_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
