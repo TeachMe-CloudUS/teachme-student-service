18fdab5aed3c7295a2a597090c9d9d68b9dbf738
Rami Nakkar <rami.nakkar00@gmail.com>
2024-12-12 19:33:11 +0100

    use shared kafka config lib

--- src/main/java/us/cloud/teachme/studentservice/infrastructure/config/KafkaConfig.java
+++ src/main/java/us/cloud/teachme/studentservice/infrastructure/config/KafkaConfig.java
@@ -1,26 +1,17 @@
 package us.cloud.teachme.studentservice.infrastructure.config;
 
 import lombok.AllArgsConstructor;
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import us.cloud.teachme.studentservice.infrastructure.messaging.KafkaTopics;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @EnableKafka
 @Configuration
 @AllArgsConstructor
+@Import(us.cloud.teachme.kafkaconfig.config.KafkaConfig.class)
 public class KafkaConfig {
 
     @Bean
@@ -32,60 +23,4 @@ public class KafkaConfig {
     public NewTopic completeCourseTopic() {
         return new NewTopic(KafkaTopics.COURSE_COMPLETED.getName(), 1, (short) 1);
     }
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "http://kafka:9092");
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(
-                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "http://kafka:9092");
-        props.put(
-                ConsumerConfig.GROUP_ID_CONFIG,
-                "teachme-student-service");
-        props.put(
-                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        props.put(
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String>
-    kafkaListenerContainerFactory() {
-
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
-
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "http://kafka:9092");
-        configProps.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        configProps.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
-
 }
