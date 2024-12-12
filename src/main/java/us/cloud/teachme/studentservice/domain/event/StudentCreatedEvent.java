16261a8f3bdf2356216149d7d6e23e3b0ea46935
Rami Nakkar <rami.nakkar00@gmail.com>
2024-12-12 20:30:38 +0100

    cibersecurity attack

--- src/main/java/us/cloud/teachme/studentservice/domain/event/StudentCreatedEvent.java
+++ src/main/java/us/cloud/teachme/studentservice/domain/event/StudentCreatedEvent.java
@@ -1,20 +1,23 @@
 package us.cloud.teachme.studentservice.domain.event;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import us.cloud.teachme.studentservice.infrastructure.messaging.KafkaTopics;
 
 import java.time.LocalDateTime;
 
-@Getter
+@Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class StudentCreatedEvent extends DomainEvent {
 
     private static final String EVENT_NAME = KafkaTopics.STUDENT_CREATED.getName();
 
-    private final String studentId;
-    private final String userId;
-    private final LocalDateTime enrollmentDate;
+    private String studentId;
+    private String userId;
+    private LocalDateTime enrollmentDate;
 
     public StudentCreatedEvent(String studentId, String userId) {
         this.studentId = studentId;
