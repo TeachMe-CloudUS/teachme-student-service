276b8936eaa96e11d4cdfb407e88117315ac1d7c
Rami Nakkar <58343300+rami-nk@users.noreply.github.com>
2025-01-03 23:10:56 +0100

    Add Swagger security requirement annotations and configurations (#30)
    Integrate `@SecurityRequirement` annotations into controllers to enforce security requirements for API endpoints. Update `application.properties` with Swagger UI and API docs paths for better documentation accessibility. Clean up unused dependency spacing in `pom.xml`.

--- src/main/java/us/cloud/teachme/studentservice/web/controller/StudentController.java
+++ src/main/java/us/cloud/teachme/studentservice/web/controller/StudentController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/students")
 @AllArgsConstructor
+@SecurityRequirement(name = "Authorization")
 @Tag(name = "Student Management", description = "APIs for managing students of the teachme platform")
 public class StudentController {
 
