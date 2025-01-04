8d9933beb765296a00eeb4ff36b1ebcb294413cf
Rami Nakkar <rami.nakkar00@gmail.com>
2025-01-04 19:06:59 +0100

    Add server configuration to OpenAPI definition
    Included a dynamic server URL in the OpenAPI definition using the `GATEWAY_SERVER_URL` environment variable with a default fallback. This enhances API documentation by reflecting the correct server URL dynamically.

--- src/main/java/us/cloud/teachme/studentservice/StudentServiceApplication.java
+++ src/main/java/us/cloud/teachme/studentservice/StudentServiceApplication.java
@@ -3,6 +3,7 @@ package us.cloud.teachme.studentservice;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -15,7 +16,8 @@ import org.springframework.kafka.annotation.EnableKafka;
                 version = "1.0",
                 description = "API for managing student data for the teachme platform",
                 contact = @Contact(name = "API Support", email = "ramnak@alum.us.es")
-        )
+        ),
+        servers = @Server(url = "${GATEWAY_SERVER_URL:http://localhost:8888}")
 )
 @EnableCaching
 @SpringBootApplication
